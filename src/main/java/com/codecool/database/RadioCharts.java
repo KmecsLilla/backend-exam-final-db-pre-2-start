bf25cd4a09c89a61baf4b9217a1bf30f3611b8aa
KmecsLilla <kmecs.lilla@gmail.com>
2021-03-24 17:52:05 +0100

    Implement db-task

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,44 @@
 package com.codecool.database;
 
 
+import java.sql.*;
+
 public class RadioCharts {
-    //TODO: implement this class
+    private String dbUrl;
+    private String dbUser;
+    private String dbPassword;
+
+    public RadioCharts(String dbUrl, String dbUser, String dbPassword) {
+        this.dbUrl = dbUrl;
+        this.dbUser = dbUser;
+        this.dbPassword = dbPassword;
+    }
+
+    public String getMostPlayedSong() {
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            String sql = "SELECT song FROM music_broadcast GROUP BY song ORDER BY SUM(times_aired) DESC LIMIT 1";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getString(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public String getMostActiveArtist() {
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            String sql = "SELECT artist FROM music_broadcast GROUP BY artist ORDER BY COUNT(DISTINCT song) DESC LIMIT 1";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getString(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
